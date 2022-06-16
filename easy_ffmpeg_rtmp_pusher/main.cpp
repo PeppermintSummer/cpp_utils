ff902c4ab79b8bcc66ab71cb62de8a7cf5fd5f98
薄荷味的夏天 <44369655+PeppermintSummer@users.noreply.github.com>
2022-06-16 19:48:56 +0800

    fix up

--- easy_ffmpeg_rtmp_pusher/main.cpp
+++ easy_ffmpeg_rtmp_pusher/main.cpp
@@ -2,8 +2,8 @@
 #include "rtmp_pusher.h"
 
 int main(){
-	auto pusher = create_RTMP_pusher("rtmp://47.100.37.207/live", width, height, 25);
-	cv::VideoCapture cap("rtsp://admin:witai515@192.168.1.231:554/h264/ch0/main/av_stream");
+	auto pusher = create_RTMP_pusher("rtmp://xxxxxxxxx/live", width, height, 25);
+	cv::VideoCapture cap("rtsp://xxxx:xxx@xxx:554/h264/ch0/main/av_stream");
 	cv::Mat image;
 	while (cap.read(image)) {
 		// do your things
